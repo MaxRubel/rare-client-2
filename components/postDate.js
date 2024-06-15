f03800dbec397f241837923fafeb71bc614533b1
braydenbubp <braydenbubp1@gmail.com>
2024-06-15 11:11:34 -0400

    mostly fixed post form

--- components/postDate.js
+++ components/postDate.js
@@ -8,12 +8,13 @@ export default function getDate() {
   const day = currentDate.getDate();
 
   // Extract the time components
-  const hours = currentDate.getHours();
-  const minutes = currentDate.getMinutes();
-  const seconds = currentDate.getSeconds();
+  // const hours = currentDate.getHours();
+  // const minutes = currentDate.getMinutes();
+  // const seconds = currentDate.getSeconds();
 
   // Create a string representation of the date and time
-  const dateTimeString = `${year}-${month < 10 ? `0${month}` : month}-${day < 10 ? `0${day}` : day} ${hours < 10 ? `0${hours}` : hours}:${minutes < 10 ? `0${minutes}` : minutes}:${seconds < 10 ? `0${seconds}` : seconds}`;
+  const dateTimeString = `${year}-${month < 10 ? `0${month}` : month}-${day < 10 ? `0${day}` : day}`;
+  //  ${hours < 10 ? `0${hours}` : hours}:${minutes < 10 ? `0${minutes}` : minutes}:${seconds < 10 ? `0${seconds}` : seconds}`;
 
   return (
     dateTimeString
