23d147e5fa1508309f86c5526758883ae26884a3
braydenbubp <braydenbubp1@gmail.com>
2024-06-14 20:30:51 -0400

    i think i made the post form correctly

--- components/forms/PostForm.js
+++ components/forms/PostForm.js
@@ -5,27 +5,45 @@ import { Button, FloatingLabel, Form } from 'react-bootstrap';
 import { createPost, updatePost } from '../../api/postData';
 import { getAllCategories } from '../../api/categories';
 import getDate from '../postDate';
+import { getAllTags } from '../../api/tags';
 
 const initialState = {
   title: '',
-  image_url: '',
+  imageUrl: '',
   category: {},
   content: '',
+  tagIds: [],
 };
 
 function PostForm({ obj }) {
   const [formInput, setFormInput] = useState(initialState);
   const [categories, setCategories] = useState([]);
+  const [tags, setTags] = useState([]);
+  const [selectedTags, setSelectedTags] = useState([]);
 
   const router = useRouter();
   const userId = localStorage.getItem('auth_token');
 
   useEffect(() => {
-    if (obj?.id) setFormInput({ ...obj, category_id: obj.category_id });
+    if (obj?.id) setFormInput({ ...obj, category: obj.category.id });
+  }, [obj]);
+
+  useEffect(() => {
+    const prevTags = [];
+    if (obj?.id) {
+      obj.tags.forEach((tag) => {
+        prevTags.push(tag.id);
+      });
+      setSelectedTags(prevTags);
+    }
   }, [obj]);
 
   useEffect(() => {
     getAllCategories().then(setCategories);
+  }, []);
+
+  useEffect(() => {
+    getAllTags().then(setTags);
   }, []);
 
   const handleChange = (e) => {
@@ -39,9 +57,11 @@ function PostForm({ obj }) {
   const handleSubmit = (e) => {
     e.preventDefault();
     if (obj?.id) {
-      updatePost(formInput).then(() => router.push('/'));
+      updatePost({ ...formInput, tags: selectedTags }).then(() => router.push('/'));
     } else {
-      const payload = { ...formInput, user_id: userId, publication_date: getDate() };
+      const payload = {
+        ...formInput, user_id: userId, publicationDate: getDate(), tags: selectedTags,
+      };
       createPost(payload).then(() => router.push('/'));
     }
   };
@@ -66,8 +86,8 @@ function PostForm({ obj }) {
           <Form.Control
             type="url"
             placeholder="Enter an image url"
-            name="image_url"
-            value={formInput.image_url}
+            name="imageUrl"
+            value={formInput.imageUrl}
             onChange={handleChange}
             required
           />
@@ -95,6 +115,22 @@ function PostForm({ obj }) {
           </Form.Select>
         </FloatingLabel>
 
+        <FloatingLabel controlId="floatingSelect" label="Tags">
+          <Form.Select
+            aria-label="Tags"
+            name="tagIds"
+            onChange={handleChange}
+            className="mb-3"
+            value={formInput.tagIds}
+          >
+            <select multiple value={selectedTags} onChange={handleChange}>
+              {tags.map((tag) => (
+                <option key={tag.id} value={tag.id}>{tag.tag}</option>
+              ))}
+            </select>
+          </Form.Select>
+        </FloatingLabel>
+
         <FloatingLabel controlId="floatingInput1" label="Post Content" className="mb-3">
           <Form.Control
             type="text"
@@ -116,10 +152,16 @@ function PostForm({ obj }) {
 PostForm.propTypes = {
   obj: PropTypes.shape({
     title: PropTypes.string,
-    image_url: PropTypes.string,
+    imageUrl: PropTypes.string,
     content: PropTypes.string,
-    category_id: PropTypes.string,
+    category: PropTypes.string,
     id: PropTypes.string,
+    tags: PropTypes.forEach(
+      PropTypes.shape({
+        id: PropTypes.number,
+        tag: PropTypes.string,
+      }),
+    ),
   }).isRequired,
 };
 
